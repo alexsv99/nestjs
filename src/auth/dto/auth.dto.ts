1065d6cc9ffaa186c63e27313f3bc9f97c9791b0
Aleksey Svalukhin <als@fozzy.com>
2024-01-09 19:23:40 +0400

    Added validation for auth dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,16 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator'
+
 export class AuthDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string
+
+  @IsString()
+  @IsNotEmpty()
   password: string
 }
